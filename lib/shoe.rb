94d1134a54d8aa6222c864ec8ef79c6c14b2a5d4
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-27 17:17:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,14 @@
 class Shoe
 
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
+
+  def cobble
+    puts "Shoe has been repaired"
+    @condition = "new"
+  end
+  
 end
